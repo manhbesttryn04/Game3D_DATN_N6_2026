8f22af3649af8a7d4166e90c8bbaacac21fbd701
Thanh Manh <manhthanhta2004@gmail.com>
2026-05-16 18:21:45 +0700

    Làm animation Player

---
+++ Assets/Hit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public Player1 owner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == owner.gameObject)
+            return;
+
+        if (other.CompareTag("Player2"))
+        {
+           Player1 p = other.GetComponent<Player1>();
+            if (p.yRotationRight)
+            {
+                p.TakeHit(true);
+            }else p.TakeHit(false);
+
+            // TODO:
+            // - damage
+            // - knockback
+            // - hitstop
+        }
+    }
+}
